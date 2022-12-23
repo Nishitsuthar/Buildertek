e915f303879cf7ab4166a9984ea0b60f0ce554eb
Nishit <nishitsuthar123@gmail.com>
2022-12-23 10:42:25 +0530

    23/12 Morning

---
+++ .sfdx/typings/lwc/apex/Create_SOV_From_Quote.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/Create_SOV_From_Quote.getQuoteData" {
+  export default function getQuoteData(param: {recordId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/Create_SOV_From_Quote.createSOV" {
+  export default function createSOV(param: {recordId: any}): Promise<any>;
+}
